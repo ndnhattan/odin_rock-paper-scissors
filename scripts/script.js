b2bae375e45cb8e9aff8319237e95f530b775447
ndnhattan <ndnhattan911@gmail.com>
2023-03-08 20:15:19 +0700

    simple UI

--- scripts/script.js
+++ scripts/script.js
@@ -1,78 +1,59 @@
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-        case 0: 
-            return "rock";
-        case 1: 
-            return "paper";
-        case 2:
-            return "scissors";
-    }
+    const arr = ["rock", "paper", "scissors"];
+    return arr[randomNumber = Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection == "rock") {
-        if (computerSelection == "rock")
-            return -1;
-        else if (computerSelection == "scissors")
-            return 11;
-        else 
-            return 01;
-    } else if (playerSelection == "paper") {
-        if (computerSelection == "paper")
-            return -1;
-        else if (computerSelection == "rock")
-            return 12;
-        else 
-            return 02;
-    } else if (playerSelection == "scissors") {
-        if (computerSelection == "scissors")
-            return -1;
-        else if (computerSelection == "paper")
-            return 13;
-        else 
-            return 03;
-    }
+    if (playerSelection == computerSelection) 
+        return 0;
+    else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+             (playerSelection == "paper" && computerSelection == "rock") ||
+             (playerSelection == "scissors" && computerSelection == "paper"))
+        return 1;
+    else 
+        return -1;
 }
 
-function game() {
-    let win = 0, lose = 0, draw = 0;
+playerSelectionList = document.querySelectorAll(".player_selection");
 
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}`);
-        let playerSelection;
-        do {
-            playerSelection = prompt("Enter your choice: ", "rock/paper/scissors");
-        } while (playerSelection.toLocaleLowerCase() != "rock" && playerSelection.toLocaleLowerCase() != "paper" && playerSelection.toLocaleLowerCase() != "scissors");
+let round = 0;
+let win = 0, lose = 0, draw = 0;
+result_div = document.querySelector(".result");
+const result_round = document.createElement("h2");
+const result_desc = document.createElement("p");
+const result_point = document.createElement("p");
+result_div.appendChild(result_round);
+result_div.appendChild(result_desc);
+result_div.appendChild(result_point);
+
+playerSelectionList.forEach((playerSelection, index) => {
+    playerSelection.addEventListener("click", () => {
+        result_round.textContent = (`Round ${round++ + 1}`);
+        if (index == 0)
+            playerSelection = "rock";
+        else if (index == 1)
+            playerSelection = "paper";
+        else 
+            playerSelection = "scissors";
         const computerSelection = getComputerChoice();
         const result = playRound(playerSelection, computerSelection);
         switch (result) {
-            case 11:
-            case 12:
-            case 13:
+            case 1:
                 win++;
-                console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+                result_desc.textContent = (`You Win! ${playerSelection} beats ${computerSelection}`);
                 break;
-            case 01:
-            case 02:
-            case 03:
+            case -1:
                 lose++;
-                console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+                result_desc.textContent = (`You Lose! ${computerSelection} beats ${playerSelection}`);
                 break;
             default: 
                 draw++;
-                console.log("Draw. :)");
+                result_desc.textContent = ("Draw :)");
         }
-    }
 
-    if (win > lose) {
-        console.log(`Congratulation!!! You Win with ${win} win, ${lose} lose`);
-    } else if (win < lose) {
-        console.log(`Sorry. You Lose with ${win} win and ${lose} lose`);
-    } else {
-        console.log(`Wow. You draw with ${win} win and ${lose} lose`);
-    }
-}
-
-game();
+        result_point.textContent = (`Point: ${win}`);
+        if (win == 5) {
+            result_point.textContent = (`Point: ${win}. Game over!`);
+        }
+    });
+});
